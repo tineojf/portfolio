337ef821e22ade3803f4dcb037666c7f6287fb32
tineojf <92743762+tineodev@users.noreply.github.com>
2026-09-03 12:52:32 -0500

    chore: configure site base path and add automated deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tineojf.github.io",
+  base: "/portfolio",
   integrations: [react()],
 });
